12dd843762a7d99b728bdd757b44cf95c0ca7f26
Veronika Kreinina <veronika.kr.0608@gmail.com>
2025-03-26 17:38:06 +0100

    check server response

--- src/server.js
+++ src/server.js
@@ -11,7 +11,6 @@ import { errorHandler } from './middlewares/errorHandler.js';
 export const setupServer = () => {
   const app = express();
   app.use(cors());
-  app.use(express.json());
   // app.use(logger);
 
   // app.use('/auth', authRouter);
