30e1524eb8914a747145c5a2755cc3e5a0925650
Veronika Kreinina <167687339+Veronika-Kreinina@users.noreply.github.com>
2025-04-14 18:45:20 +0200

    +

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -47,13 +47,7 @@ export const getContactsController = async (req, res) => {
 export const getContactByIdController = async (req, res) => {
   const { contactId } = req.params;
 
-
   const contact = await getOneContact(contactId, req.user._id);
-
-
-  // if (contact.userId.toString() !== req.user.id.toString()) {
-  //   throw new createHttpError.Forbidden('Contact is not allowed');
-  // }
 
   if (!contact) {
     throw new createHttpError(404, `Contact with id:${contactId} not found`);
@@ -116,7 +110,6 @@ export const upsertContactController = async (req, res) => {
 
   const { isNew, data } = await updateContact(
     contactId,
-
     req.user._id,
     updatedData,
 
@@ -136,8 +129,6 @@ export const upsertContactController = async (req, res) => {
 
 export const patchContactController = async (req, res) => {
   const { contactId } = req.params;
-
-  const result = await updateContact(contactId, req.user._id, req.body);
 
   let photo;
 
@@ -159,7 +150,6 @@ export const patchContactController = async (req, res) => {
     ...(photo && { photo }),
   };
   const result = await updateContact(contactId, req.user._id, updatedData);
-
 
   if (!result) {
     throw new createHttpError(404, 'Contact with id:${contactId} not found');
