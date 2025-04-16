290052c03ecd2f10c72ba5187ced9bf0663036a9
Veronika Kreinina <167687339+Veronika-Kreinina@users.noreply.github.com>
2025-04-16 17:05:58 +0200

    Update saveToUploads.js
    remove "src" from saveFileToUploads

--- src/utils/saveToUploads.js
+++ src/utils/saveToUploads.js
@@ -3,7 +3,7 @@ import path from 'node:path';
 import { getEnvVar } from './getEnvVar.js';
 
 export const saveFileToUploads = async (file) => {
-  await fs.rename(file.path, path.resolve('src', 'uploads', file.filename));
+  await fs.rename(file.path, path.resolve( 'uploads', file.filename));
 
   return `${getEnvVar('APP_DOMAIN')}/uploads/${file.filename}`;
 };
